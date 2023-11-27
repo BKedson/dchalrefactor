ece31f2c667a2a8f70420d266ba28cc817282214
Nofer <sxx261@case.edu>
2023-11-26 23:02:28 -0500

    Added Loot Generation

--- Assets/Scripts/Dungeon Generation/DungeonGenerator.cs
+++ Assets/Scripts/Dungeon Generation/DungeonGenerator.cs
@@ -1,5 +1,5 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public enum MathTopic
@@ -7,41 +7,44 @@ public enum MathTopic
     Addition, Subtraction, Multiplication, Division, Fraction
 }
 
+[Serializable]
+struct LootType
+{
+    [SerializeField] public string name;
+    [SerializeField] public GameObject lootPrefab;
+    [SerializeField] public float generationChance;
+}
+
+struct MapUnit
+{
+    public bool roomPlanned;
+    public int lootTypeIdx;
+}
+
 public class DungeonGenerator : MonoBehaviour
 {
     public static DungeonGenerator _instance;
 
+    [SerializeField] private Transform dungeonRoot;
+
+    [Header("Room Generation")]
     [SerializeField] private GameObject roomPrefab;
     [SerializeField] private GameObject doorPrefab;
     [SerializeField, Min(2), Tooltip("Min / Max")] private Vector2Int numRoomPerLv;
     [SerializeField, Min(2)] private int maxRecursionDepth;
     [SerializeField] private float roomOffset;
-    [SerializeField] private Transform dungeonRoot;
-    //[SerializeField] private LayerMask whatIsRoom;
+
+    [Header("Loot Generation")]
+    [SerializeField] LootType[] lootTypes;
 
     [Header("Exit Generation")]
     [SerializeField] private GameObject exitPrefab;
 
-    //[Header("Normal Loot")]
-    //[Range(0f, 1f)]
-    //[SerializeField] private float normalLootChance;
-    //[SerializeField] private GameObject normalLootPrefab;
-
-    //[Header("Rare Loot")]
-    //[Range(0f, 1f)]
-    //[SerializeField] private float rareLootChance;
-    //[SerializeField] private GameObject rareLootPrefab;
-
-    //[Header("Clue")]
-    //[Range(0f, 1f)]
-    //[SerializeField] private float clueChance;
-    //[SerializeField] private GameObject cluePrefab;
-
     [Header("Math Operation")]
-    [SerializeField] private MathTopic mathOperation;
+    [SerializeField] private MathTopic mathTopic;
 
     private int numRoomPlanned = 0;
-    private bool[,] map;
+    private MapUnit[,] map;
 
     // Start is called before the first frame update
     void Awake()
@@ -61,12 +64,12 @@ public class DungeonGenerator : MonoBehaviour
 
     public MathTopic GetTopic()
     {
-        return mathOperation;
+        return mathTopic;
     }
 
     public void SetTopic(MathTopic op)
     {
-        mathOperation = op;
+        mathTopic = op;
     }
 
     public void InitializeDungeon()
@@ -88,8 +91,16 @@ public class DungeonGenerator : MonoBehaviour
         while (numRoomPlanned < numRoomPerLv.x)
         {
             numRoomPlanned = 0;
-            map = new bool[numRoomPerLv.y, numRoomPerLv.y];
-            map[0, 0] = true;
+            map = new MapUnit[numRoomPerLv.y, numRoomPerLv.y];
+            for (int i = 0; i < numRoomPerLv.y; i++)
+            {
+                for (int j = 0; j < numRoomPerLv.y; j++)
+                {
+                    map[i, j].roomPlanned = false;
+                    map[i, j].lootTypeIdx = -1;
+                }
+            }
+            map[0, 0].roomPlanned = true;
             numRoomPlanned++;
             RecursivePlanRoom(Vector2Int.zero);
         }
@@ -98,7 +109,7 @@ public class DungeonGenerator : MonoBehaviour
         {
             for (int j = 0; j < numRoomPerLv.y; j++)
             {
-                if (map[i, j]) GenRoom(i, j);
+                if (map[i, j].roomPlanned) GenRoom(i, j);
             }
         }
 
@@ -110,55 +121,60 @@ public class DungeonGenerator : MonoBehaviour
 
     private void RecursivePlanRoom(Vector2Int currPos, int depth = 1)
     {
-        if (depth > maxRecursionDepth) return;
+        if (depth > maxRecursionDepth || numRoomPlanned >= numRoomPerLv.y) return;
         // Front
-        if (numRoomPlanned < numRoomPerLv.y && Random.Range(0.0f, 1.0f) < 0.8f)
+        if (UnityEngine.Random.Range(0.0f, 1.0f) < 0.8f)
         {
             Vector2Int temp = currPos;
             temp.y++;
-            if (!map[temp.x, temp.y])
-            {
-                map[temp.x, temp.y] = true;
-                numRoomPlanned++;
-                RecursivePlanRoom(temp, depth + 1);
-            }
+            if (!map[temp.x, temp.y].roomPlanned) { PlanRoom(temp, depth); }
         }
         // Right
-        if (numRoomPlanned < numRoomPerLv.y && Random.Range(0.0f, 1.0f) < 0.6f)
+        if (UnityEngine.Random.Range(0.0f, 1.0f) < 0.6f)
         {
             Vector2Int temp = currPos;
             temp.x++;
-            if (!map[temp.x, temp.y])
-            {
-                map[temp.x, temp.y] = true;
-                numRoomPlanned++;
-                RecursivePlanRoom(temp, depth + 1);
-            }
+            if (!map[temp.x, temp.y].roomPlanned) { PlanRoom(temp, depth); }
         }
         // Left
-        if (numRoomPlanned < numRoomPerLv.y && Random.Range(0.0f, 1.0f) < 0.5f)
+        if (UnityEngine.Random.Range(0.0f, 1.0f) < 0.5f)
         {
             Vector2Int temp = currPos;
             temp.x--;
-            if (temp.x >= 0 && !map[temp.x, temp.y])
-            {
-                map[temp.x, temp.y] = true;
-                numRoomPlanned++;
-                RecursivePlanRoom(temp, depth + 1);
-            }
+            if (temp.x >= 0 && !map[temp.x, temp.y].roomPlanned) { PlanRoom(temp, depth); }
         }
         // Back
-        if (numRoomPlanned < numRoomPerLv.y && Random.Range(0.0f, 1.0f) < 0.5f)
+        if (UnityEngine.Random.Range(0.0f, 1.0f) < 0.5f)
         {
             Vector2Int temp = currPos;
             temp.y--;
-            if (temp.y >= 0 && !map[temp.x, temp.y])
+            if (temp.y >= 0 && !map[temp.x, temp.y].roomPlanned) { PlanRoom(temp, depth); }
+        }
+    }
+
+    private void PlanRoom(Vector2Int temp, int depth)
+    {
+        map[temp.x, temp.y].roomPlanned = true;
+        numRoomPlanned++;
+
+        if (numRoomPlanned == numRoomPerLv.y)
+        {
+            map[temp.x, temp.y].lootTypeIdx = -2;
+        }
+        else
+        {
+            float genChance = UnityEngine.Random.Range(0.0f, 1.0f);
+            for (int i = 0; i < lootTypes.Length; i++)
             {
-                map[temp.x, temp.y] = true;
-                numRoomPlanned++;
-                RecursivePlanRoom(temp, depth + 1);
+                if (genChance < lootTypes[i].generationChance)
+                {
+                    map[temp.x, temp.y].lootTypeIdx = i;
+                    break;
+                }
             }
         }
+
+        RecursivePlanRoom(temp, depth + 1);
     }
 
     public void DestroyDungeon()
@@ -171,53 +187,6 @@ public class DungeonGenerator : MonoBehaviour
         numRoomPlanned = 0;
     }
 
-    //private Tuple<string, string> GenMathQA(MathType t)
-    //{
-    //    int a, b;
-    //    switch (t)
-    //    {
-    //        case MathType.Addition:
-    //            a = UnityEngine.Random.Range(0, 300);
-    //            b = UnityEngine.Random.Range(0, 300);
-    //            return Tuple.Create(a + " + " + b + " = ?", (a + b).ToString());
-    //        case MathType.Subtraction:
-    //            a = UnityEngine.Random.Range(0, 300);
-    //            b = UnityEngine.Random.Range(0, 200);
-    //            return Tuple.Create(a + " - " + b + " = ?", (a - b).ToString());
-    //        case MathType.Multiplication:
-    //            a = UnityEngine.Random.Range(0, 10);
-    //            b = UnityEngine.Random.Range(0, 10);
-    //            return Tuple.Create(a + " X " + b + " = ?", (a * b).ToString());
-    //        case MathType.Division:
-    //            a = UnityEngine.Random.Range(1, 10);
-    //            b = UnityEngine.Random.Range(1, 10);
-    //            return Tuple.Create(a * b + " / " + b + " = ?", a.ToString());
-    //        case MathType.Fraction:
-    //            a = UnityEngine.Random.Range(0, 10);
-    //            b = UnityEngine.Random.Range(1, 10);
-    //            int k = UnityEngine.Random.Range(2, 10);
-    //            string q = a * k + "/" + b * k + " = ?\n\n";
-    //            string ans = a + "/" + b;
-    //            int currectAnsIdx = UnityEngine.Random.Range(1, 4);
-    //            for (int i = 1; i < 5; i++)
-    //            {
-    //                if (i == currectAnsIdx)
-    //                {
-    //                    q += "  " + i + ". " + ans + "  ";
-    //                }
-    //                else
-    //                {
-    //                    a = UnityEngine.Random.Range(0, 100);
-    //                    b = UnityEngine.Random.Range(1, 10);
-    //                    q += "  " + i + ". " + a + "/" + b + "  ";
-    //                }
-    //            }
-    //            return Tuple.Create(q, currectAnsIdx.ToString());
-    //    }
-
-    //    return Tuple.Create("", "");
-    //}
-
     public void GenRoom(int x, int z)
     {
         Vector3 targetPos = new Vector3(x * roomOffset, 0f, z * roomOffset);
@@ -225,18 +194,33 @@ public class DungeonGenerator : MonoBehaviour
         GameObject room = Instantiate(roomPrefab, dungeonRoot);
         room.transform.position = targetPos;
 
+        if (map[x, z].lootTypeIdx == -2)
+        {
+            Instantiate(exitPrefab, targetPos + Vector3.up, Quaternion.identity, room.transform);
+        }
+        else if (map[x, z].lootTypeIdx > -1)
+            Instantiate(lootTypes[map[x, z].lootTypeIdx].lootPrefab, targetPos + Vector3.up, Quaternion.identity, room.transform);
+
         // Left
-        if (x > 0 && map[x - 1, z])
-            Instantiate(doorPrefab, targetPos, Quaternion.Euler(0f, 270f, 0f), room.transform);
+        if (x > 0 && map[x - 1, z].roomPlanned)
+        {
+            GameObject doorObj = Instantiate(doorPrefab, targetPos, Quaternion.Euler(0f, 270f, 0f), room.transform);
+        }
         // Right
-        if (x < numRoomPerLv.y - 1 && map[x + 1, z])
-            Instantiate(doorPrefab, targetPos, Quaternion.Euler(0f, 90f, 0f), room.transform);
+        if (x < numRoomPerLv.y - 1 && map[x + 1, z].roomPlanned)
+        {
+            GameObject doorObj = Instantiate(doorPrefab, targetPos, Quaternion.Euler(0f, 90f, 0f), room.transform);
+        }
         // Back
-        if (z > 0 && map[x, z - 1])
-            Instantiate(doorPrefab, targetPos, Quaternion.Euler(0f, 180f, 0f), room.transform);
+        if (z > 0 && map[x, z - 1].roomPlanned)
+        {
+            GameObject doorObj = Instantiate(doorPrefab, targetPos, Quaternion.Euler(0f, 180f, 0f), room.transform);
+        }
         // Front
-        if (z < numRoomPerLv.y - 1 && map[x, z + 1])
-            Instantiate(doorPrefab, targetPos, Quaternion.Euler(0f, 0f, 0f), room.transform);
+        if (z < numRoomPerLv.y - 1 && map[x, z + 1].roomPlanned)
+        {
+            GameObject doorObj = Instantiate(doorPrefab, targetPos, Quaternion.identity, room.transform);
+        }
     }
 
     //private void OnDrawGizmos()
