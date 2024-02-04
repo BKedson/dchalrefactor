5421c16b807df9c9b7047500e746de74a17082ae
sneakysnail187 <maduvivier53@gmail.com>
2024-02-04 17:05:36 -0500

    cleanup

--- Assets/Refactored Scripts/Player/PlayerDataManager.cs
+++ Assets/Refactored Scripts/Player/PlayerDataManager.cs
@@ -1,19 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-// TODO: Rename when refactor is complete
-public class NewPlayerDataManager : MonoBehaviour
+public static class PlayerDataManager : object
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    //stores the different player fields, name, score
+    private static string name = "PLAYER";
+    private static int score;
+    private static int wingScore;
+    private static List<float> times;
+    private static List<float> wingTimes;
+
+    static PlayerDataManager(){
+        times = new List<float>();
+        wingTimes = new List<float>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    //methods for updating and returning the fields
+    public static void UpdateTime(float timeSegment){
+        //adjust avg times
+        times.Add(timeSegment);
+        wingTimes.Add(timeSegment);
+    }
+    public static void UpdateName(string nameInput){
+        //update the name
+        name = nameInput;
+    }
+    public static void UpdateScore(int scoreInput){
+        //update the wing score
+        score = scoreInput;
+    }
+
+    public static void UpdateWingScore(int scoreInput){
+        //update the wing score
+        wingScore = scoreInput;
+    }
+
+    public static void resetTime(){
+        wingTimes.Clear();
+    }
+
+    public static float getTime(){
+        return times.Sum()/times.Count;
+    }
+
+    public static float getWingTime(){
+        return wingTimes.Sum()/wingTimes.Count;
+    }
+
+    public static string getName(){
+        //return the name
+        return name;
+    }
+
+    public static int getScore(){
+        //return the score
+        return score;
+    }
+    public static int getWingScore(){
+        //return the score
+        return wingScore;
     }
 }
