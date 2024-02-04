005e2d913ccac4c812ad1c92bdfdbd22d860f2c3
BKedson <brandonkedson@gmail.com>
2024-02-04 15:11:19 -0500

    Initial main menu and transition manager setup

--- Assets/Refactored Scripts/TransitionManager.cs
+++ Assets/Refactored Scripts/TransitionManager.cs
@@ -1,9 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TransitionManager : MonoBehaviour
 {
+
+    public static TransitionManager transitionMan;
+
+    private void Awake() {
+        // Don't destroy the transition manager on load, but do destroy if there are duplicates
+        if(transitionMan) {
+            DestroyImmediate(gameObject);
+        } else {
+            DontDestroyOnLoad(gameObject);
+            transitionMan = this;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +29,30 @@ public class TransitionManager : MonoBehaviour
     {
         
     }
+
+    public void ContinueGame() {
+        if (PlayerPrefs.HasKey("CurrentLevel")) {
+            SceneManager.LoadScene(PlayerPrefs.GetString("CurrentLevel"));
+        } else {
+            PlayerPrefs.SetString("CurrentLevel", "Test Combat Room");
+            SceneManager.LoadScene("Test Combat Room");
+        }
+    }
+
+    public void NewGame() {
+        SceneManager.LoadScene("Test Combat Room");
+    }
+
+    public void Options() {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void Credits() {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+
 }
