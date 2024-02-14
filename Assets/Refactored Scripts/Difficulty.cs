6b3dc1642b05076ce7dbc87bc5ad6e85c35764e0
BKedson <brandonkedson@gmail.com>
2024-02-13 19:08:47 -0500

    Implement options menus

---
+++ Assets/Refactored Scripts/Difficulty.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Defines legal difficulties and assigns them a numeric value
+public enum Difficulty
+{
+    Easy = 0,
+    Medium = 1,
+    Hard = 2
+}
