3ad33ac385e19c508c4fa63f7c7894a6d153bf58
adrianakamor <atk88@case.edu>
2024-05-12 15:34:33 -0400

    Fixed Animation SFX

--- Assets/Scripts/Sound Scripts/EnemySound.cs
+++ Assets/Scripts/Sound Scripts/EnemySound.cs
@@ -1,23 +1,23 @@
 using UnityEngine;
 
-public class PlayerSound : MonoBehaviour
+public class EnemySound : MonoBehaviour
 {
     public AudioSource audioSource;
     public AudioClip[] audioClips;
 
 
 
-    public void PlayerWalk()
+    public void EnemyWalk()
     {
         audioSource.PlayOneShot(audioClips[0]);
     }
 
-    public void PlayerJump()
+    public void EnemyRun()
     {
         audioSource.PlayOneShot(audioClips[1]);
     }
 
-    public void PlayerAttack()
+    public void EnemyAttack()
     {
         audioSource.PlayOneShot(audioClips[2]);
     }
