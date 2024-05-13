d577a46970ba2563e2dcc73ccd94aa468b85e8f5
adrianakamor <atk88@case.edu>
2024-05-13 13:01:59 -0400

    Made enemy SFX only heard when in close proximity to enemy

--- Assets/Scripts/Sound Scripts/EnemySound.cs
+++ Assets/Scripts/Sound Scripts/EnemySound.cs
@@ -4,8 +4,24 @@ public class EnemySound : MonoBehaviour
 {
     public AudioSource audioSource;
     public AudioClip[] audioClips;
+    public float maxVol = 1f;
+    public float minVol = 0f;
+    public float proximity = 8000f;
+    private Transform obj;
 
+    void Start() 
+    {
+        obj = GameObject.FindGameObjectWithTag("Player").transform;
+    }
 
+    void Update() 
+    {
+        if(obj == null) return;
+
+        float distance = Vector3.Distance(transform.position, obj.position);
+        float volume = Mathf.Lerp(maxVol, minVol, distance/proximity);
+        audioSource.volume = volume;
+    }
 
     public void EnemyWalk()
     {
@@ -21,4 +37,6 @@ public class EnemySound : MonoBehaviour
     {
         audioSource.PlayOneShot(audioClips[2]);
     }
+
+
 }
