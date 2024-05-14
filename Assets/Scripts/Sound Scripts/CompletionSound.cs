2878befc9bd0ef398f90d75c651454c91adb5f58
adrianakamor <atk88@case.edu>
2024-05-14 15:13:05 -0400

    Level Completion SFX Debugging

--- Assets/Scripts/Sound Scripts/CompletionSound.cs
+++ Assets/Scripts/Sound Scripts/CompletionSound.cs
@@ -6,21 +6,33 @@ public class CompletionSound : MonoBehaviour
 {
     public AudioClip completeSound;
     private AudioSource audioSource;
+    private bool enemiesGenerated;
+    private bool updated;
 
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        enemiesGenerated = false;
+        updated = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (GameObject.FindGameObjectsWithTag("enemy").Length < 1)
+        if (GameObject.FindGameObjectsWithTag("enemy").Length >= 1 && updated == false)
+        {
+            enemiesGenerated = true;
+            updated = true;
+        }
+
+        if (GameObject.FindGameObjectsWithTag("enemy").Length < 1 && enemiesGenerated == true)
 		{
             if (!audioSource.isPlaying)
             {
                 audioSource.PlayOneShot(completeSound);
+                enemiesGenerated = false;
+                updated = false;
             }
             else
             {
@@ -36,5 +48,7 @@ public class CompletionSound : MonoBehaviour
             yield return null;
         }
         audioSource.PlayOneShot(completeSound);
+        enemiesGenerated = false;
+        updated = false;
     }
 }
